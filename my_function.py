66f1d5ddde0447cca86641c771999571f2d00a82
Luqmaan Sahar <luqmaan.sahar@gmail.com>
2025-10-20 15:33:54 +0100

    added docstrings

--- my_function.py
+++ my_function.py
@@ -1,5 +1,11 @@
 def my_function():
+    """
+    A function to display a greeting
+    """
     print("Hello from my_function!")
 
 def new_function():
+    """
+    displays a short description of a new function
+    """
     print("This is a new function, created on a new branch!")
